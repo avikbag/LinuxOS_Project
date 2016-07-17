eb8d79b33eb64fefaab41fd9bdefe1219f868934
Tyler Boswell <tjb322@tux64-14.cs.drexel.edu>
2016-07-17 19:25:48 -0400

    move wrapper files for myjoin

---
+++ P2/testSysMyjoin.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+        if (argc != 2){
+                printf("Usage: test_myjoin $PID\n");
+                exit(0);
+        }
+
+        long pid = atol(argv[1]);
+
+        if(pid){
+                printf("Returned %d\n", syscall(290, pid));
+        }
+        else{
+                printf("Usage: test_myjoin $PID\n");
+                exit(0);
+        }
+}
